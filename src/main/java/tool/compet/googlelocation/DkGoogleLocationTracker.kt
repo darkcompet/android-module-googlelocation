5246c8aa60b044fa3ef923ca9d8e1ec584d0c0c8
DarkCompet <darkcompet@gmail.com>
2022-08-27 17:08:30 +0700

    Convert to kotlin

---
+++ src/main/java/tool/compet/googlelocation/DkGoogleLocationTracker.kt
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2017-2020 DarkCompet. All rights reserved.
+ */
+package tool.compet.googlelocation
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.location.Location
+import android.os.Looper
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationListener
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.Task
+import tool.compet.core.DkConst
+import tool.compet.core.DkLogcats
+import tool.compet.core.DkUtils
+
+/**
+ * Differentiate with [tool.compet.location.DkLocationTracker], this location tracker
+ * use Google location service to tracking user's location.
+ *
+ * Ref: https://github.com/android/location-samples
+ */
+@SuppressLint("MissingPermission")
+class DkGoogleLocationTracker(host: Activity?, listener: Listener) : LocationCallback(), LocationListener {
+	interface Listener {
+		fun onLastLocationUpdated(location: Location)
+		fun onLocationResult(locations: List<Location>)
+		fun onLocationChanged(location: Location)
+	}
+
+	private val locationProviderClient: FusedLocationProviderClient
+
+	/**
+	 * Obtain location request instance for more setting, for eg,. interval, fastestInterval, priority,...
+	 */
+	val locationRequest: LocationRequest
+	private val listeners = ArrayList<Listener>()
+
+	init {
+		val locationRequest = LocationRequest.create()
+		locationRequest.interval = 30000 // default 30 s
+		locationRequest.fastestInterval = 10000 // default 10 s
+		locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+		listeners.add(listener)
+
+		this.locationRequest = locationRequest
+		locationProviderClient = LocationServices.getFusedLocationProviderClient(host!!)
+
+		requestLastLocation(host)
+	}
+
+	override fun onLocationResult(result: LocationResult) {
+		for (listener in listeners) {
+			listener.onLocationResult(result.locations)
+		}
+	}
+
+	override fun onLocationChanged(location: Location) {
+		for (listener in listeners) {
+			listener.onLocationChanged(location)
+		}
+	}
+
+	fun register(listener: Listener): DkGoogleLocationTracker {
+		if (!listeners.contains(listener)) {
+			listeners.add(listener)
+		}
+		return this
+	}
+
+	fun unregister(listener: Listener): DkGoogleLocationTracker {
+		listeners.remove(listener)
+		return this
+	}
+
+	/**
+	 * Start location updates.
+	 */
+	fun start(host: Activity?) {
+		if (!DkUtils.checkPermission(host, DkConst.ACCESS_FINE_LOCATION, DkConst.ACCESS_COARSE_LOCATION)) {
+			DkLogcats.warning(this, "Skip request location updates since NO permission granted !")
+			return
+		}
+		locationProviderClient.requestLocationUpdates(locationRequest, this, Looper.getMainLooper())
+	}
+
+	/**
+	 * Stop location updates.
+	 */
+	fun stop() {
+		locationProviderClient.removeLocationUpdates(this)
+	}
+
+	/**
+	 * Request last known location of user.
+	 */
+	fun requestLastLocation(host: Activity?) {
+		if (!DkUtils.checkPermission(host, DkConst.ACCESS_FINE_LOCATION, DkConst.ACCESS_COARSE_LOCATION)) {
+			DkLogcats.warning(this, "Skip request last location since NO permission granted !")
+			return
+		}
+		locationProviderClient.lastLocation.addOnCompleteListener(host!!) { task: Task<Location?> ->
+			if (task.isSuccessful && task.result != null) {
+				for (listener in listeners) {
+					listener.onLastLocationUpdated(task.result!!)
+				}
+			}
+			else {
+				DkLogcats.warning(this, "Failed to get last location, task: $task")
+			}
+		}
+	}
+}
